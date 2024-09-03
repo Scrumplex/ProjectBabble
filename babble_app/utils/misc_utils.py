041f05c36a2ac66e1fdc1c19ec21fbf10b98e32c
Sefa Eyeoglu <contact@scrumplex.net>
2024-09-03 18:03:53 +0200

    fixup! refactor
    Signed-off-by: Sefa Eyeoglu <contact@scrumplex.net>

--- babble_app/utils/misc_utils.py
+++ babble_app/utils/misc_utils.py
@@ -1,11 +1,15 @@
 import os
-from pygrabber.dshow_graph import FilterGraph
-
 is_nt = True if os.name == "nt" else False
-graph = FilterGraph()
+if is_nt:
+    from pygrabber.dshow_graph import FilterGraph
+
+    graph = FilterGraph()
 
 
 def list_camera_names():
+    if not is_nt:
+        return []
+
     cam_list = graph.get_input_devices()
     cam_names = []
     for index, name in enumerate(cam_list):
@@ -14,6 +18,9 @@ def list_camera_names():
 
 
 def get_camera_index_by_name(name):
+    if not is_nt:
+        return None
+
     cam_list = graph.get_input_devices()
 
     for i, device_name in enumerate(cam_list):
